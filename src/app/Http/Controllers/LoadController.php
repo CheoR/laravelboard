9ee3fed019ffa9d9dd31482864ed117182cf169b
CheoR <cheo.roman+github@gmail.com>
2023-05-24 21:07:36 -0500

    build: landing page

--- src/app/Http/Controllers/LoadController.php
+++ src/app/Http/Controllers/LoadController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Load;
 
 class LoadController extends Controller
 {
-    //
+    public function index() {
+        return view('loads.index', [
+            'header' => 'Welcome',
+        ]);
+    }
 }
