472a08163c86550d831c9c41b17d812f62412925
CheoR <cheo.roman+github@gmail.com>
2023-05-26 08:21:27 -0500

    style: add partials

--- src/resources/views/loads/index.blade.php
+++ src/resources/views/loads/index.blade.php
@@ -1,5 +1,8 @@
 <x-layout>
-    <div>
+    @include('partials._hero')
+    @include('partials._search')
+    <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
         <h1>{{ $header }}</h1>
     </div>
+    <p>No loads found</p>
 </x-layout>
